79fc11ec05b52f84be45ed59dca8582f0c1be562
Adam Clayman <adam@clayman.org>
2018-09-03 04:23:23 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,5 @@
 class Owner
-  OWNERS = []
-
   def initialize
-    OWNERS << self
-  end
-
-  def self.all
-    OWNERS
-  end
-
-  def self.reset_all
-    OWNERS.clear
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 end
