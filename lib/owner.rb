f4a7e35212a600dc55344a45dc1820505d28de81
Adam Clayman <adam@clayman.org>
2018-09-03 04:25:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,4 +4,19 @@ class Owner
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
   end
+
+  def buy_cat(name)
+    new_pet = Cat.new(name)
+    pets << new_pet
+  end
+  
+  def buy_dog(name)
+    new_pet = Dog.new(name)
+    pets << new_pet
+  end
+  
+  def buy_fish(name)
+    new_pet = Fish.new(name)
+    pets << new_pet
+  end
 end
